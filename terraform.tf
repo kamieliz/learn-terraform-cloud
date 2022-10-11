f6f59f1e0bef1d0d5a3007bfd50f1cfea5e05ee1
Kamaria Hardy <kamehardy@gmail.com>
2022-10-11 13:02:36 -0400

    Comment out cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
 terraform {
-	
+	/*
   cloud {
     organization = "Kam-demo-work"
 
@@ -7,7 +7,7 @@ terraform {
       name = "learn-terraform-cloud"
     }
   } 
-
+*/
 
   required_providers {
     aws = {
